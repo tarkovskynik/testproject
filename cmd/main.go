1d98a2578a4f6a4590418197dd773406c5be194d
Tarkovskyi <tarkovskynik@gmail.com>
2023-03-07 18:29:56 +0200

    get pairs from binance api

---
+++ cmd/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"sync"
+
+	"github.com/aiviaio/go-binance/v2"
+)
+
+func main() {
+	var (
+		apiKey    = "your api key"
+		secretKey = "your secret key"
+		ctx       = context.Background()
+		wg        sync.WaitGroup
+	)
+	ch := make(chan map[string]string)
+
+	client := binance.NewClient(apiKey, secretKey)
+
+	resp, err := client.NewExchangeInfoService().Do(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var coins []string
+	for _, s := range resp.Symbols {
+		if strings.Contains(s.Symbol, "USDT") {
+			coins = append(coins, s.Symbol)
+		}
+		if len(coins) == 5 {
+			break
+		}
+	}
+
+	for _, c := range coins {
+		go func(symbol string) {
+			result := make(map[string]string)
+			prices, err := client.NewListPricesService().Symbol(symbol).Do(ctx)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			result[symbol] = prices[0].Price
+
+			ch <- result
+		}(c)
+	}
+
+	for {
+		select {
+		case pair, ok := <-ch:
+			if !ok {
+				wg.Done()
+				return
+			}
+			for s, v := range pair {
+				fmt.Println(s + " " + v)
+			}
+		}
+	}
+}
